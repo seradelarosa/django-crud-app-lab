7d616dabd19d093ad3774933135c8b5b1132281d
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-31 19:23:22 -0400

    morning skincare routine displays yay

--- cleanslate/main_app/urls.py
+++ cleanslate/main_app/urls.py
@@ -3,4 +3,7 @@ from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('morning/', views.morning, name='morning'),
+    path('evening/', views.morning, name='evening'),
+    path('weekly/', views.morning, name='weekly'),
 ]
--- cleanslate/main_app/views.py
+++ cleanslate/main_app/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-
-# main_app/views.py
-
-from django.shortcuts import render
-
 # Import HttpResponse to send text-based responses
 from django.http import HttpResponse
+from .fixtures.skincare_data import skincare_products
+
 
 # Define the home view function
 def home(request):
     # Send a simple HTML response
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html')
 
+def morning(request):
+    # for product in skincare_products:
+    #     product.image_url = f"{product.image}"
+
+    return render(request, 'skincare/morning.html', {'skincare_products': skincare_products})
+
+# def my_view(request):
+#     context = {
+#         'class_a': class_a_instances,
+#         'class_b': class_b_instances,
+#         'class_c': class_c_instances,
+#     }
+#     return render(request, 'myapp/template.html', context)
